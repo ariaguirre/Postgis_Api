f10353c8d3566f691bda99872b7f55c20996bd0c
ariaguirre <ariaguirre64@gmail.com>
2023-07-20 14:48:06 -0300

    adding stations

--- client/src/components/Home.vue
+++ client/src/components/Home.vue
@@ -1,6 +1,21 @@
 <template>
     <div class="home">
-      <p>{{ message }}</p>
+      <table>
+        <thead>
+          <tr>
+            <th>Id</th>
+            <th>Nombre</th>
+            <th>Ubicación</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="station in stations" :key="station.id">
+            <td>{{ station.id }}</td>
+            <td>{{ station.name }}</td>
+            <td>{{ station.location }}</td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   </template>
   
@@ -8,24 +23,24 @@
   import axios from 'axios'
   export default {
     name: 'Main', 
-    data () {
-        return{
-            message: 'New message from VueJS.'
-        }
+    data() {
+      return {
+        stations: []  // Array para almacenar las estaciones obtenidas del servidor
+      }
     },
     methods: {
-        getMessage() {
-            const path = 'http://localhost:5000/message'
-            axios.get(path).then((response) => {
-                this.message = response.data
-            })
-            .catch((error) => {
-                console.log(error);
-            })
-        }
+      getStations() {
+        const path = 'http://localhost:5000/'
+        axios.get(path).then((response) => {
+          this.stations = response.data // Almacenar los datos en la variable stations
+        })
+        .catch((error) => {
+          console.log(error);
+        })
+      }
     }, 
-    created(){
-        this.getMessage()
+    created() {
+      this.getStations()
     }
   }
   </script>
--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/components/Home'
-
+/* eslint-disable */
 Vue.use(Router)
 
 export default new Router({
